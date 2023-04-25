7219a64ef81eca1abade26a362f35db13694675a
Juliya1305 <131388549+Juliya1305@users.noreply.github.com>
2023-04-25 19:46:10 +0300

    Add files via upload

--- Pin-Pong.py
+++ Pin-Pong.py
@@ -1,98 +1,123 @@
-#Создай собственный Шутер!
 from pygame import *
-from random import randint
-from time import time as timer
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, sprite_image, player_x, player_y, speed, wight, height):
+    def __init__(self, sprite_image, player_x, player_y, speed_x, speed_y, wight, height):
         super().__init__()
         self.image = transform.scale(image.load(sprite_image), (wight, height))
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
-        self.speed = speed
+        self.speed_x = speed_x
+        self.speed_y = speed_y
 
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-
     def update_l(self):
         key_pressed = key.get_pressed()
         
         if key_pressed[K_w] and self.rect.y > 0:
-            self.rect.y -= self.speed
+            self.rect.y -= self.speed_y
 
-        if key_pressed[K_s] and self.rect.y < 500:
-            self.rect.y += self.speed
-
+        if key_pressed[K_s] and self.rect.y < 400:
+            self.rect.y += self.speed_y
     
+    def update_r(self):
+        key_pressed = key.get_pressed()
         
-window = display.set_mode((700, 500))
+        if key_pressed[K_UP] and self.rect.y > 0:
+            self.rect.y -= self.speed_y
+
+        if key_pressed[K_DOWN] and self.rect.y < 400:
+            self.rect.y += self.speed_y
+
+class Ball(GameSprite):
+
+    def update(self):
+        global count1
+        global count2
+
+        
+        ball.rect.x += ball.speed_x
+        ball.rect.y += ball.speed_y
+        if sprite.collide_rect(ball, player1):
+            self.speed_x *= -1
+
+        if sprite.collide_rect(ball, player2):
+            self.speed_x *= -1
+
+        if self.rect.x <= 0:
+            self.speed_x *= -1
+            count2 += 1
+        
+        if self.rect.x >= 500:
+            self.speed_x *= -1
+            count1 += 1
+    
+        if self.rect.y == 0 or self.rect.y == 500:
+            self.speed_y *= -1
+        
+window = display.set_mode((600, 600))
 display.set_caption('Пин-Понг')
 
 #задай фон сцены
-#background = transform.scale(image.load('galaxy.jpg'), (700,500))
+background = transform.scale(image.load('background.png'), (600,600))
 
 font.init()
-font1 = font.Font(None, 30)
-font2 = font.Font(None, 70)
-win = font2.render('YOU WIN!', True, (255, 215, 0))
-lose = font2.render('YOU LOSE!', True, (255, 0, 0))
+font1 = font.SysFont('Arial', 30)
+font2 = font.SysFont('Arial', 70)
 
-count = 0
-counts = font1.render('Счет:' + str(count), True, (255, 255, 255))
+win1 = font2.render('Player1 win!', True, (255, 215, 0))
+win2 = font2.render('Player2 win!', True, (255, 215, 0))
 
-mixer.init()
-#mixer.music.load('space.ogg')
-#mixer.music.play()
+count1 = 0
+count2 = 0
+counts = font1.render('Счет: ' + str(count1) + ' : ' + str(count2), True, (255, 255, 255))
 
-#fire = mixer.Sound('fire.ogg')
 clock = time.Clock()
 
-player = Player('left.png', 0, 250, 10, 90, 90)
+player1 = Player('left.png', 0, 250, 10, 10, 50, 200)
+player2 = Player('right.png', 550, 250, 10, 10, 50, 200)
+ball = Ball('ball.png', 325, 340, 5, 5,100, 100)
+
+players = sprite.Group()
+players.add(player1)
+players.add(player2)
 
 run = True
 finish = False
-
+list_player1 = list()
 while run:
 
     for e in event.get():
         if e.type == QUIT:
             run = False
 
-        if e.type == KEYDOWN:
-            if e.key == K_SPACE and energy:
-                player.fire()
-                num_fire += 1
-                if num_fire % 5 == 0:
-                    energy = False
-                    time1 =  timer()
-            
-
     if finish != True: 
-       # window.blit(background,(0, 0))
+        window.blit(background,(0, 0))
 
-        player.reset()
+        player1.reset()
+        player2.reset()
+        ball.reset()
 
-        player.update_l()
+        player1.update_l()
+        player2.update_r()
+        ball.update()
+
         
 
-        if  count >= 5:
-            window.blit(win, (245, 250))
+        if count1 >= 5:
+            window.blit(win1,(120, 250))
             finish = True
-
         
-        counts = font1.render('Счет:' + str(count), True, (255, 255, 255))
-
-
-        #sprites_list = sprite.spritecollide(player, monsters, False)
-        #asteroid_list = sprite.spritecollide(player, asteroids, False)
-
-        #sprites_list2  = sprites_list
-        #asteroid_list2  = asteroid_list
-
+        if count2 >= 5:
+            window.blit(win2,(120, 250))
+            finish = True
         
+
+        counts = font1.render('Счет: ' + str(count1) + ' : ' + str(count2), True, (255, 255, 255))
+
         window.blit(counts,(0, 30))
         
     clock.tick(60)
